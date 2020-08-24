d93f8d81163830a3b7d4f75ae416e9770c9f10de
Diwash Rai <diwash.rai1993@gmail.com>
2020-08-24 23:30:52 +0100

    initial commit

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // turn off buffer sync between c and c++ streams. Decouple cin and cout
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
+
+    int n;
+    cin >> n;
+
+    cerr << "time taken: " << (float)clock()/CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
