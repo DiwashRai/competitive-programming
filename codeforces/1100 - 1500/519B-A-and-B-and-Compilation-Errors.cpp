884ced9baf65de2247e43d069619b9ec8d879723
Diwash Rai <diwash.rai1993@gmail.com>
2021-03-28 23:36:39 +0100

    Completed the problem from yesterday

--- codeforces/1100 - 1500/519B-A-and-B-and-Compilation-Errors.cpp
+++ codeforces/1100 - 1500/519B-A-and-B-and-Compilation-Errors.cpp
@@ -15,17 +15,35 @@ int main() {
     int n;
     cin >> n;
 
-    int a[10000] = {1000};
-    int b[10000] = {1000};
-    int c[10000] = {1000};
+    int a[100000];
+    int b[100000];
+    int c[100000];
 
     for (int i = 0; i < n; ++i) cin >> a[i];
     for (int i = 0; i < n - 1; ++i) cin >> b[i];
     for (int i = 0; i < n - 2; ++i) cin >> c[i];
 
     sort(a, a + n);
-    sort(b, b + n);
-    sort(c, c + n);
+    sort(b, b + n - 1);
+    sort(c, c + n - 2);
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (a[i] != b[i])
+        {
+            cout << a[i] << endl;
+            break;
+        }
+    }
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if (b[i] != c[i])
+        {
+            cout << b[i] << endl;
+            break;
+        }
+    }
 
     cerr << "time taken: " << (float)clock()/CLOCKS_PER_SEC << " secs" << endl;
     return 0;
