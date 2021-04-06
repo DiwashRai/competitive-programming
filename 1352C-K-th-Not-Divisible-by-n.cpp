2eb7bfff374dd31aaac15192a7e411cfbc73a3ed
Diwash Rai <diwash.rai1993@gmail.com>
2021-04-06 22:11:43 +0100

    started new problem. Too much to do for work to complete today

---
+++ 1352C-K-th-Not-Divisible-by-n.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // turn off buffer sync between c and c++ streams. Decouple cin and cout
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; ++i)
+    {
+        int n, k;
+        cin >> n >> k;
+    }
+
+    cerr << "time taken: " << (float)clock()/CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
